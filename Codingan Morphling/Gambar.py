12e1bdab060c396264d5ac349aaeadc5cc2896f5
Ahmad Fadillah <77627326+AhmadFadillah12@users.noreply.github.com>
2022-05-09 19:30:59 +0700

    Add files via upload

---
+++ Codingan Morphling/Gambar.py
@@ -0,0 +1,31 @@
+import pygame
+
+#sementara code gambar di satukan sama code gamenya sebelum dipisah setelah fix gambarnya
+Gambar_Dino_Awal   =    pygame.image.load('Gambarrr/dino_idle.png')
+Gambar_Dino_Nunduk =   [pygame.image.load('Gambarrr/Dino/Dino Mati/Dead (2).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Mati/Dead (3).png')] # sementara
+
+Gambar_Dino_Lari   =   [pygame.image.load('Gambarrr/Dino/Dino Lari/Run (1).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (2).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (3).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (4).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (5).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (6).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (7).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (8).png')]
+
+Gambar_Dino_Melompat = [pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (1).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (2).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (3).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (4).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (5).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (6).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (7).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (8).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (9).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (10).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (11).png'),
+                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (12).png')]
+
+Gambar_Obstacle_Batu = [pygame.image.load('Gambarrr/Batu/Crystal2.png'),
+                        pygame.image.load('Gambarrr/Batu/Crystal4.png')]
