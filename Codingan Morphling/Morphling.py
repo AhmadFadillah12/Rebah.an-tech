7b8bb975232eb720d1ee2dab7a6add8605ea8c24
Ahmad Fadillah <77627326+AhmadFadillah12@users.noreply.github.com>
2022-05-09 19:30:37 +0700

    Update Morphling.py

--- Codingan Morphling/Morphling.py
+++ Codingan Morphling/Morphling.py
@@ -17,36 +17,6 @@ background = pygame.transform.scale(background,(950,836))
 background_menu = pygame.image.load('Gambarrr/menu_start.png')
 background_menu = pygame.transform.scale(background_menu,(950,836))
 
-#sementara code gambar di satukan sama code gamenya sebelum dipisah setelah fix gambarnya
-Gambar_Dino_Awal   =    pygame.image.load('Gambarrr/dino_idle.png')
-Gambar_Dino_Nunduk =   [pygame.image.load('Gambarrr/Dino/Dino Mati/Dead (2).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Mati/Dead (3).png')] # sementara
-
-Gambar_Dino_Lari   =   [pygame.image.load('Gambarrr/Dino/Dino Lari/Run (1).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (2).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (3).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (4).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (5).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (6).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (7).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lari/Run (8).png')]
-
-Gambar_Dino_Melompat = [pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (1).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (2).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (3).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (4).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (5).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (6).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (7).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (8).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (9).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (10).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (11).png'),
-                        pygame.image.load('Gambarrr/Dino/Dino Lompat/Jump (12).png')]
-
-Gambar_Obstacle_Batu = [pygame.image.load('Gambarrr/Batu/Crystal2.png'),
-                        pygame.image.load('Gambarrr/Batu/Crystal4.png')]
-
 Gambar_Obstacle_Batu[0] = pygame.transform.scale(Gambar_Obstacle_Batu[0],(100,100))
 Gambar_Obstacle_Batu[1] = pygame.transform.scale(Gambar_Obstacle_Batu[1],(100,100))
 pohon = pygame.image.load('Gambarrr/Pohon/7.png')
